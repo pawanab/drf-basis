ee97812af812327a781110f5b1bb32acdb66d260
Pawan Arora <pawan.arora@myoperator.co>
2020-05-15 16:25:34 +0530

    class based serializer

--- sendy/urls.py
+++ sendy/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from sendy import views
+from sendy.views import SnippetList, SnippetDetail
 
 urlpatterns = [
-    path('', views.snippet_list),
-    path('/<int:pk>', views.snippet_detail)
+    path('', SnippetList.as_view()),
+    path('/<int:pk>', SnippetDetail.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
